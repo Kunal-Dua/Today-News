a36bf0bed3806f9e84984dddfdf9bf25ab32fa8f
Kunal Dua <kunaldua2000@gmail.com>
2022-06-24 13:44:49 +0530

    adding news and newsItem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar.js'
+import Heading from './Components/Heading';
+import NewsItem from './Components/NewsItem'
+import React, { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor="#030d7e";
+      console.log("dark mode enabled");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor="white";
+      console.log("light mode enabled");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar title="News Today" mode={mode} toggleMode={toggleMode} />
+      <Heading heading="News Today" mode={mode}/>
+      <NewsItem/>
     </div>
   );
 }
