07243bfdb544e3adbeecb2a899106bc74c919e91
Kunal Dua <kunaldua2000@gmail.com>
2022-06-30 01:47:38 +0530

    class to className

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,7 +5,7 @@ export default function NewsItem(props) {
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
 				<p className="card-text">{props.description}</p>
-				<p class="card-text"><small class="text-muted">By {props.author?props.author:"Unknown"} on {new Date (props.date).toGMTString()}</small></p>
+				<p className="card-text"><small className="text-muted">By {props.author?props.author:"Unknown"} on {new Date (props.date).toGMTString()}</small></p>
 				<a href={props.newsUrl} className="btn btn-primary" target="_blank" rel="noreferrer">
 					Read More
 				</a>
