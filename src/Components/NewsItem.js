079ef810ceb7afd9c3ca6d9888e577b4b41183a4
Kunal Dua <kunaldua2000@gmail.com>
2022-06-30 01:08:16 +0530

    Revert "sources addded"
    This reverts commit 9e07020f1d07d72bdb3a51cdc4733f2dee043665.

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,16 +1,13 @@
 export default function NewsItem(props) {
 	return (
-		<div className="card">
+    <div className="card">
 			<img src={props.imgUrl} className="card-img-top" alt="..." />
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
 				<p className="card-text">{props.description}</p>
-				<p class="card-text"><small class="text-muted">By {props.author ? props.author : "Unknown"} on {new Date(props.date).toGMTString()}</small></p>
+				<p class="card-text"><small class="text-muted">By {props.author?props.author:"Unknown"} on {new Date (props.date).toGMTString()}</small></p>
 				<a href={props.newsUrl} className="btn btn-primary" target="_blank" rel="noreferrer">
 					Read More
-					<span class="position-absolute translate-middle badge rounded-pill bg-danger" style={{left : "88%"}}>
-						{props.source?props.source:"Unknown"}<span class="visually-hidden">Source</span>
-					</span>
 				</a>
 			</div>
 		</div>
