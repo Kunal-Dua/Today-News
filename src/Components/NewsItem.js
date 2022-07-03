a71843ea29a435afbbadf08e0d67ff05c34cf47d
Kunal Dua <kunaldua2000@gmail.com>
2022-07-04 00:03:02 +0530

    removed unnecessary files

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,7 +6,6 @@ export default function NewsItem(props) {
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
 				<p className="card-text">{props.description}</p>
-				
 				<p className="card-text"><small className="text-muted">By {props.author?props.author:"Unknown"} on {new Date (props.date).toGMTString()}</small></p>
 				<a href={props.newsUrl} className="btn btn-primary" target="_blank" rel="noreferrer">
 					Read More
